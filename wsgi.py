964d279cb71943848333116374ce530579123573
oneteatree <oneteatree@gmail.com>
2020-08-01 17:18:27 +0000

    first commit

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import random, string
+from app.main import app
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
